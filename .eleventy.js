fa8052a3529c40fb00b6014552042934f243e978
pm <macchioni@users.noreply.github.com>
2025-04-25 09:11:02 +0200

    css meglio?

--- .eleventy.js
+++ .eleventy.js
@@ -3,13 +3,16 @@ const { DateTime } = require("luxon");
 module.exports = function (eleventyConfig) {
   // Filtro "readableDate": formato semplice (es. 25 Apr 2025)
   eleventyConfig.addFilter("readableDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
+    return DateTime.fromJSDate(dateObj).toFormat("dd LLL<ctrl3348>");
   });
 
-  // Filtro "formatDate": formato personalizzabile (default: yyyy-MM-dd)
+  // Filtro "formatDate": formato personalizzabile (default:<ctrl3348>-MM-dd)
   eleventyConfig.addFilter("formatDate", (dateObj, format = "yyyy-MM-dd") => {
     return DateTime.fromJSDate(dateObj).toFormat(format);
   });
+
+  // Copia la cartella "assets" nella radice della cartella di output
+  eleventyConfig.addPassthroughCopy("./src/assets");
 
   // Directory di input/output
   return {
