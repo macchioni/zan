26ff142337a8d7362a531c601a758b721fff566c
pm <macchioni@users.noreply.github.com>
2025-04-25 18:48:55 +0200

    dicho

--- .eleventy.js
+++ .eleventy.js
@@ -20,6 +20,20 @@ module.exports = function (eleventyConfig) {
     return collectionApi.getFilteredByGlob("src/blog/posts/*.md").reverse();
   });
 
+  eleventyConfig.addFilter("dichotomize", function(collection) {
+    let result = {};
+    
+    // Itera su tutte le collezioni
+    for (let key in collection) {
+      // Salta la collezione "all"
+      if (key !== "all") {
+        result[key] = collection[key];
+      }
+    }
+    
+    return result;
+  });
+
   return {
     dir: {
       input: "src",
