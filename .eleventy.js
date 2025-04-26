7dffdd7b958a7de1fdf2f622b4f208ac30c0de24
pm <macchioni@users.noreply.github.com>
2025-04-26 14:00:11 +0200

    tag2

--- .eleventy.js
+++ .eleventy.js
@@ -11,55 +11,59 @@ module.exports = function (eleventyConfig) {
     return slugify(input, { lower: true, remove: /[*+~.()'"!:@]/g });
   });
 
+  // Filtro per data leggibile
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
   });
 
+  // Filtro per formattazione date
   eleventyConfig.addFilter("formatDate", (dateObj, format = "yyyy-MM-dd") => {
     return DateTime.fromJSDate(dateObj).toFormat(format);
   });
 
-  // Collezioni per i feed RSS
-  eleventyConfig.addCollection("journal", function(collection) {
+  // Collezione journal
+  eleventyConfig.addCollection("journal", function (collection) {
     return collection.getFilteredByGlob("src/journal/*.md").reverse();
   });
 
-  eleventyConfig.addCollection("links", function(collection) {
+  // Collezione links
+  eleventyConfig.addCollection("links", function (collection) {
     return collection.getFilteredByGlob("src/links/*.md").reverse();
   });
 
-  // Crea una collezione di tag con conteggi, escludendo quelli finti
-  eleventyConfig.addCollection("tagList", function(collection) {
+  // Collezione posts
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/blog/posts/*.md").reverse();
+  });
+
+  // Collezione dei tag filtrando quelli "falsi"
+  eleventyConfig.addCollection("tagList", function (collection) {
     let tagSet = {};
 
-    collection.getAll().forEach(function(item) {
+    collection.getAll().forEach(function (item) {
       if ("tags" in item.data) {
         let tags = item.data.tags;
 
-        tags.filter(tag => !["posts", "all", "tagList", "journal", "links"].includes(tag)).forEach(tag => {
-          if (!tagSet[tag]) {
-            tagSet[tag] = [];
-          }
-          tagSet[tag].push(item);
-        });
+        tags.filter(tag => !["all", "posts", "tagList", "journal", "links"].includes(tag))
+            .forEach(tag => {
+              if (!tagSet[tag]) {
+                tagSet[tag] = [];
+              }
+              tagSet[tag].push(item);
+            });
       }
     });
 
     return tagSet;
   });
 
-  // Filtro per affettare array
-  eleventyConfig.addFilter("slice", function(array, start, end) {
+  // Filtro per affettare gli array
+  eleventyConfig.addFilter("slice", function (array, start, end) {
     return array.slice(start, end);
   });
 
-  eleventyConfig.addPassthroughCopy("./src/assets");
-
-  eleventyConfig.addCollection("posts", function (collectionApi) {
-    return collectionApi.getFilteredByGlob("src/blog/posts/*.md").reverse();
-  });
-
-  eleventyConfig.addFilter("dichotomize", function(collection) {
+  // Filtro per eliminare "all" da una collezione
+  eleventyConfig.addFilter("dichotomize", function (collection) {
     let result = {};
 
     for (let key in collection) {
@@ -76,7 +80,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("dateToRfc3339", pluginRss.dateToRfc3339);
   eleventyConfig.addFilter("getNewestCollectionItemDate", pluginRss.getNewestCollectionItemDate);
 
-  // Shortcode per buildTime
+  // Shortcode per la buildTime
   eleventyConfig.addShortcode("buildTime", () => {
     return new Date().toLocaleString("it-IT", {
       year: "numeric",
@@ -87,7 +91,15 @@ module.exports = function (eleventyConfig) {
     });
   });
 
+  // Copia statica degli assets
+  eleventyConfig.addPassthroughCopy("./src/assets");
+
+  // Configurazione finale
   return {
     dir: {
       input: "src",
       output: "_site",
+      includes: "_includes"
+    }
+  };
+};
