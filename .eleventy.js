0586c5075b9ddfbfbe2fcb93ff00980976d4fe5f
pm <macchioni@users.noreply.github.com>
2025-04-28 23:21:57 +0200

    correction

--- .eleventy.js
+++ .eleventy.js
@@ -11,7 +11,7 @@ module.exports = function(eleventyConfig) {
     if (!value) return value;
 
     // Funzione per sostituire apostrofi solo fuori da tag HTML
-    return value.replace(/(>)([^<]*?)('/g, "$1$2’");
+    return value.replace(/(>)([^<]*?)('|’)/g, "$1$2’");
   });
 
   // Filtro per slug URL-friendly
