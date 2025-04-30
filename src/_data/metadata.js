476cb742ba935db0e6ecc89f2e685008ae78ce6f
pm <macchioni@users.noreply.github.com>
2025-04-30 14:32:43 +0200

    image oG

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -1,3 +1,8 @@
 module.exports = {
-    url: "https://shiny-tiramisu-55122b.netlify.app"
+    url: "https://shiny-tiramisu-55122b.netlify.app",
+    title: "Piero Macchioni",
+    description: "Journal",
+    language: "it-it", "en-en",
+    // Immagine predefinita per tutto il sito
+    image: "/assets/images/pi.png" 
   };
