008cdbf0223e8955a83dbf209763fcff9f1328e1
pm <macchioni@users.noreply.github.com>
2025-04-30 15:12:57 +0200

    arrray lingue

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
     url: "https://shiny-tiramisu-55122b.netlify.app",
     title: "Piero Macchioni",
     description: "Journal",
-    language: "it-it", "en-en",
+    language: ["it-it", "en-en"],
     // Immagine predefinita per tutto il sito
     image: "/assets/images/pi.png" 
   };
